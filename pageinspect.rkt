2d1f09e5271d59cea7be8e2e332a86ab1835d2a0
Hadi Moshayedi <hadi@moshayedi.net>
2020-05-02 21:14:14 -0700

    implement get-heap-tuples

--- pageinspect.rkt
+++ pageinspect.rkt
@@ -1,10 +1,13 @@
 #lang racket
 
-(require db)
+(require db
+         db/util/postgresql)
 
 (provide (all-defined-out))
 
+;;
 ;; data structures
+;;
 
 (struct page-header (lsn
                      checksum
@@ -15,6 +18,25 @@
                      pagesize
                      version
                      prune_xid))
+
+;; corresponds to ItemIdData in itemid.h
+(struct item-id (lp_off
+                 lp_flags
+                 lp_len))
+
+;; corresponds to HeapTupleHeaderData in htup_details.h
+(struct htup-header (t_xmin
+                     t_xmax
+                     t_field3
+                     t_ctid
+                     t_infomask2
+                     t_infomask
+                     t_hoff
+                     t_bits))
+
+(struct heap-tuple (itemid
+                    header
+                    attrs))
 
 ;; exported functions
 
@@ -50,6 +72,17 @@
                relname fork idx))
   (apply page-header (vector->list result)))
 
+(define (get-heap-tuples pgc relname fork idx)
+  (define query "SELECT lp_off, lp_flags, lp_len, t_xmin::text, t_xmax::text,
+                        t_field3, t_ctid::text, t_infomask2, t_infomask, t_hoff, t_bits, t_attrs
+                 FROM heap_page_item_attrs(get_raw_page($1, $2, $3), $1::regclass)")
+  (for/list ([row-v (query-rows pgc query relname fork idx)])
+    (define row (vector->list row-v))
+    (define itemid (apply item-id (take row 3)))
+    (define header (apply htup-header (take (drop row 3) 8)))
+    (define attrs (pg-array->list (car (drop row 11))))
+    (heap-tuple itemid header attrs)))
+
 ;; Finds maximum value v in [min, max] which (satisifies? v)
 ;; If not found, default is returned.
 (define (upperbound satisfies? min max default)
@@ -61,12 +94,24 @@
       [else (bs min (- mid 1) best)]))
   (bs min max default))
 
+(define (hex-format a-number)
+  (define digits "0123456789ABCDEF")
+  (string #\0 #\x
+          (string-ref digits (quotient a-number 16))
+          (string-ref digits (remainder a-number 16))))
+
+(define (bytes->hex bs)
+  (string-join (map hex-format (bytes->list bs)) " "))
+
 (define (test)
   (define pgc
     (postgresql-connect #:user "hadi"
                         #:database "postgres"))
   (displayln (bytes-length (get-raw-page pgc "t" "main" 0)))
   (displayln (relation-page-count pgc "pg_class" "main"))
-  (displayln (page-header-pagesize (get-page-header pgc "pg_class" "main" 0))))
+  (displayln (page-header-pagesize (get-page-header pgc "pg_class" "main" 0)))
+  (define pages (map heap-tuple-attrs (get-heap-tuples pgc "t" "main" 0)))
+  (define first-page (map bytes->hex (first pages)))
+  (displayln first-page))
 
 (test)
